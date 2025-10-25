fa61c5d39e4b97ec7f1090eb974eb945519d6ecc
Chris Prosser <chris.prosser@rhul.ac.uk>
2025-10-25 14:33:28 +0100

    Added vline/hline geom default update

--- R/update_geom_defaults.R
+++ R/update_geom_defaults.R
@@ -1,12 +1,15 @@
 #' Sensible geom defaults
 #' @export
 use_chris_geom_defaults <- function() {
-  ggplot2::update_geom_defaults("point", list(size = 2, alpha = 0.5))
+  ggplot2::update_geom_defaults("point", list(size = 2))
   ggplot2::update_geom_defaults("line",  list(linewidth = 0.7))
   ggplot2::update_geom_defaults("col",   list(linewidth = 0))
   ggplot2::update_geom_defaults("bar",   list(linewidth = 0))
   ggplot2::update_geom_defaults("errorbar",   list(width = 0))
-  ggplot2::update_geom_defaults("ribbon",   list(linewidth = 0, alpha = .5))
+  ggplot2::update_geom_defaults("ribbon",   list(linewidth = 0, alpha = .25))
+  ggplot2::update_geom_defaults("vline",   list(linewidth = 0.5, colour = "grey50", linetype = "22"))
+  ggplot2::update_geom_defaults("hline",   list(linewidth = 0.5, colour = "grey50", linetype = "22"))
+
   invisible(TRUE)
 }
 
